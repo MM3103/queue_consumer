3bbfd8005d5b67505ab61befb38e688b5b641da6
MM3103 <misha.makhalin@yandex.ru>
2022-10-14 22:35:09 +0300

    Initial commit

---
+++ src/main/java/liga/medical/MedicalQueueConsumerApplication.java
@@ -0,0 +1,13 @@
+package liga.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalQueueConsumerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MedicalQueueConsumerApplication.class, args);
+    }
+}
