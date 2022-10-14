e2fa5a2c45cce3faa09fe888ec239a5d7b99be0f
MM3103 <misha.makhalin@yandex.ru>
2022-10-14 23:51:57 +0300

    Hometask-4

---
+++ src/main/java/liga/medical/model/QueueNames.java
@@ -0,0 +1,10 @@
+package liga.medical.model;
+
+public class QueueNames {
+
+    public static final String DAILY_QUEUE_NAME = "daily_queue";
+
+    public static final String ALERT_QUEUE_NAME = "alert_queue";
+
+    public static final String ERROR_QUEUE_NAME = "error_queue";
+}
